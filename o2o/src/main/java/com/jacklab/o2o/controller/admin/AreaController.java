bf1ea9234f0db280ef1932aa98e03105bdb7c96e
jack <uplinkxu@gmail.com>
2018-08-12 11:58:47 -0400

    add log

--- o2o/src/main/java/com/jacklab/o2o/controller/admin/AreaController.java
+++ o2o/src/main/java/com/jacklab/o2o/controller/admin/AreaController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +16,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.jacklab.o2o.entity.Area;
 import com.jacklab.o2o.service.AreaService;
 
+
 @Controller
 @RequestMapping("/admin")
 public class AreaController {
 
+	Logger logger= LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	private AreaService areaService;
 
 	@RequestMapping(value = "/listarea", method = RequestMethod.GET)
 	@ResponseBody
 	private Map<String, Object> listArea() {
+		logger.info("===start===");
+		long startTime = System.currentTimeMillis();
 		Map<String, Object> modelMap = new HashMap<>();
 		List<Area> list = new ArrayList<>();
 		try {
@@ -36,6 +43,9 @@ public class AreaController {
 			modelMap.put("success", Boolean.FALSE);
 			modelMap.put("errMsg", e.toString());
 		}
+		long endTime=System.currentTimeMillis();
+		logger.debug("cost time:{}ms",(endTime-startTime));
+		logger.info("===end===");
 		return modelMap;
 	}
 }
